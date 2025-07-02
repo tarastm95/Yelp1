c067d66c59799259830b1476b86dca6a6ec4a345
Taras Mazepa <34683038+tarastm95@users.noreply.github.com>
2025-07-02 05:06:14 +0300

    Update default API base URL to server

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,11 +2,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+  const API_BASE = process.env.REACT_APP_API_BASE_URL || 'http://46.62.139.177:8000';
   // 1) Proxy to your Django backend
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8000',
+      target: API_BASE,
       changeOrigin: true,
     })
   );
