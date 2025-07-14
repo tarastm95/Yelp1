08219b3efd309cc20d6e2db2f4510e7342c00f94
Taras Mazepa <34683038+tarastm95@users.noreply.github.com>
2025-07-15 02:16:25 +0300

    Fix migration for LeadPendingTask text field

--- backend/webhooks/migrations/0048_leadpendingtask_text.py
+++ backend/webhooks/migrations/0048_leadpendingtask_text.py
@@ -1,4 +1,10 @@
 from django.db import migrations, models
+from django.db.models import F
+
+
+def set_initial_text(apps, schema_editor):
+    LeadPendingTask = apps.get_model("webhooks", "LeadPendingTask")
+    LeadPendingTask.objects.all().update(text=F("task_id"))
 
 class Migration(migrations.Migration):
     dependencies = [
@@ -12,6 +18,7 @@ class Migration(migrations.Migration):
             field=models.TextField(default=""),
             preserve_default=False,
         ),
+        migrations.RunPython(set_initial_text, reverse_code=migrations.RunPython.noop),
         migrations.AddConstraint(
             model_name="leadpendingtask",
             constraint=models.UniqueConstraint(
